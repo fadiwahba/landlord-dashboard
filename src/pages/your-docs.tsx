dbb5a80ea5f0e15ba138074a1dfebb8663dc59bc
Fady Soliman <fady.soliman@valocity.co.nz>
2021-08-15 16:11:32 +1200

    Adding lorem text to pages and fixing layout style

--- src/pages/your-docs.tsx
+++ src/pages/your-docs.tsx
@@ -3,8 +3,12 @@ import Nav from "../components/Nav";
 
 export default function YourDocs() {
   return (
-    <section className="page_wrapper">
+    <>
       <h1>Your Documents Page!</h1>
-    </section>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo accusamus vel earum ipsa, odit cum, unde vero voluptates maxime laudantium sapiente nulla illum dignissimos commodi corporis qui ut atque reiciendis?
+        Laudantium iusto quo quos recusandae expedita laboriosam nostrum neque fugiat doloremque? Provident ad magni quas facere exercitationem eos accusantium nam ipsa corporis maiores! Quae, tempora accusantium obcaecati esse corrupti quibusdam.
+      </p>
+    </>
   );
 }
